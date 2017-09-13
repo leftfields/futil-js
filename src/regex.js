558fe2744cb5328dbfba4aced7842ba356334107
Your Name <jonathan@meekmachine.org>
2017-09-13 01:13:48 -0400

    Issue #142 -- Updated matchAnyWord and matchAllWords to use curried string operators. This seems to be faster and more relaible when working with large strings.

--- src/regex.js
+++ src/regex.js
@@ -5,19 +5,40 @@ export const testRegex = _.curry((regex, str) => (new RegExp(regex)).test(str))
 export const makeRegex = options => text => RegExp(text, options)
 export const makeAndTest = options => _.flow(makeRegex(options), testRegex)
 
-export const matchAnyWord = _.flow(
+const anyWordToRegexp = _.flow(
   _.words,
-  _.map(makeAndTest('gi')),
-  _.overSome
+  _.join('|')
 )
 
-export const matchAllWords = _.flow(
+const wordsToRegexp = _.flow(
   _.words,
-  _.map(makeAndTest('gi')),
-  _.overEvery
+  _.map(x => `(?=.*${x})`),
+  _.join('')
 )
 
-export const postings = _.curry((regex, str) => {
+
+const _contains = _.curry((body, token) => body.indexOf(token) > -1)
+
+const matchWords = _.curry((fn, body, accumulator, token) => fn(_contains(body), accumulator, token))
+
+const matchAny = matchWords((contains, accumulator, token) => !accumulator ? contains(token) : true)
+
+const matchAll = matchWords((contains, accumulator, token) => (!accumulator ?
+                                                       (accumulator === undefined) ?
+                                                         contains(token) : false
+                                                      : contains(token)))
+export const matchAllWords = _.curry((query, body) => {
+  var match = matchAll(body.toLowerCase())
+  var matches = _.words(query.toLowerCase()).reduce(match);
+  return !!matches;
+})
+export const matchAnyWord = _.curry((query, body) => {
+  var match = matchAny(body.toLowerCase())
+  var matches = _.words(query.toLowerCase()).reduce(match);
+  return !!matches;
+})
+
+export const postings = (regex, str) => {
   var match = regex.exec(str)
   let result = []
 
@@ -26,13 +47,13 @@ export const postings = _.curry((regex, str) => {
     match = regex.exec(str)
   }
   return result
-})
+}
 
-export const postingsForWords = _.curry((string, str) => _.reduce(
+export const postingsForWords = (string, str) => _.reduce(
   (result, word) => push(postings(RegExp(word, 'gi'), str), result), []
-)(_.words(string)))
+)(_.words(string))
 
-export const highlightFromPostings = _.curry((start, end, postings, str) => {
+export const highlightFromPostings = (start, end, postings, str) => {
   let offset = 0
   _.each(posting => {
     str = insertAtIndex(posting[0] + offset, start, str)
@@ -41,7 +62,7 @@ export const highlightFromPostings = _.curry((start, end, postings, str) => {
     offset += end.length
   }, mergeRanges(postings))
   return str
-})
+}
 
-export const highlight = _.curry((start, end, pattern, input) =>
-  highlightFromPostings(start, end, _.flatten(postingsForWords(pattern, input)), input))
+export const highlight = (start, end, pattern, input) =>
+  highlightFromPostings(start, end, _.flatten(postingsForWords(pattern, input)), input)
